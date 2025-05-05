0e994479ff6e8bf3fa000a1f8f23390535aad0a7
March Sevenia Putri Surga <221210053@student.mercubuana-yogya.ac.id>
2025-05-05 13:56:38 +0700

    update package(instalasi react-boostrap)

--- magang-frontend/src/App.jsx
+++ magang-frontend/src/App.jsx
@@ -1,31 +1,25 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Footer from "./components/Footer";
 import PendaftaranMagang from "./pages/user/PendaftaranMagang";
-<<<<<<< HEAD
 import Beranda from "./pages/Beranda";
 import StatusMagang from "./pages/user/StatusMagang";
-=======
 import Dashboard from "./pages/Dashboard";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> origin/rani
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const location = useLocation();
   return (
     <>
       <Routes>
-<<<<<<< HEAD
         <Route path="/" element={<Beranda />} />
         <Route path="/pendaftaran-magang" element={<PendaftaranMagang />} />
         <Route path="/status-magang" element={<StatusMagang />} />
-=======
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/admin" element={<Dashboard />} />
         <Route path="/pendaftaran-magang" element={<PendaftaranMagang />} />
->>>>>>> origin/rani
       </Routes>
       <Footer />
     </>
